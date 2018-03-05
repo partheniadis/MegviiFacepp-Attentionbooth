e30b43bbbd604701cb702cb3f5fe89ea716342e4
partheniadis <partheniadis.k@gmail.com>
2018-03-05 20:59:11 +0100

    Initial commit

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "Ay9DPinuQYWWFXOm_LXuVjUFapAF5Ypd";
+	public static String API_SECRET = "t7cbLvKSPhCcreT-oDYEdt6prsXdebsC";
+	
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v2/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v2/auth";
+}
